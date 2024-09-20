4403b5d952132cb6e0bca817e2412230a7b80fe5
ocamilinho <adrianocamilo055@gmail.com>
2024-09-19 22:00:16 -0300

    initial commit

---
+++ src/main/java/com/ocamilinho/picpay_v3/PicpayV3Application.java
@@ -0,0 +1,13 @@
+package com.ocamilinho.picpay_v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayV3Application.class, args);
+	}
+
+}
